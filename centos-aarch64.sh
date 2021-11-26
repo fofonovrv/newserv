eb2b3ad4c62b855d5250a8c58e2a2646afd6f060
fofonovrv <92461655+fofonovrv@users.noreply.github.com>
2021-11-26 20:12:11 +0300

    Update centos-aarch64.sh

--- centos-aarch64.sh
+++ centos-aarch64.sh
@@ -3,6 +3,9 @@
 sudo dnf update -y
 sudo dnf clean all
 
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+
+sudo dnf install -y nano vim wget curl net-tools lsof bash-completion
 sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
 sudo dnf install -y docker-ce
 sudo systemctl start docker
@@ -10,6 +13,7 @@ sudo systemctl enable docker
 sudo usermod -aG docker ${USER}
 sudo systemctl stop firewalld
 sudo systemctl disable firewalld
+
 #Activate for arm64!!!
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
